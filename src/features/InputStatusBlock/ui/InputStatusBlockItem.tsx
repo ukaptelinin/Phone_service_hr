4cb09bc64300c7f598cbf4ae4112a783f4123f6a
Kaptelinin <100-21@mail.ru>
2026-06-17 22:33:48 +0300

    Radio button styles have been fixed

--- src/features/InputStatusBlock/ui/InputStatusBlockItem.tsx
+++ src/features/InputStatusBlock/ui/InputStatusBlockItem.tsx
@@ -1,18 +1,20 @@
 import { Label, Radio } from '@heroui/react';
 import type { FC } from 'react';
+
 interface StatusBlockItemsProps {
   title: string;
   color: string;
   value: string;
 }
+
 export const InputStatusBlockItem: FC<StatusBlockItemsProps> = ({ title, color, value }) => {
   return (
-    <Radio value={value}>
-      <Radio.Control className={`border-2 ${color} shrink-0 `}>
+    <Radio value={value} className="items-center!">
+      <Radio.Control className={`border-2 ${color} shrink-0`}>
         <Radio.Indicator />
       </Radio.Control>
       <Radio.Content className="flex-1">
-        <Label className="whitespace-normal wrap-break-word max-w-[20ch]">{title}</Label>
+        <Label className="whitespace-normal wrap-break-word max-w-[22ch]">{title}</Label>
       </Radio.Content>
     </Radio>
   );
